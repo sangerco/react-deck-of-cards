42a4fc10b916d4f1a77c38413ba1425f7bfdf4fc
sangerco <112724308+sangerco@users.noreply.github.com>
2023-03-07 12:54:09 -0700

    'finished positioning and styling'

--- src/Deck.js
+++ src/Deck.js
@@ -1,25 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import './Deck.css';
 
-const Deck = ({ deckId }) => {
-    const [ card, setCard] = useState(null);
-    useEffect(() => {
-        async function getCard() {
-            const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
-            setCard(res.data.cards[0].image);
-        }
-        getCard()
-    }, [card]);
+const Deck = ({ image }) => {
 
     return (
-        <div>
-            <div>
-                <button>Gimme a card!</button>
-            </div>
-            <div>
-                <img src={card} />
-            </div>  
+        <div className='Deck-card'>
+            <img src={image} />
         </div>
+
     )
 }
 
--- src/DeckWrapper.js
+++ src/DeckWrapper.js
@@ -1,23 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Deck from './Deck';
+import './DeckWrapper.css';
 import axios from 'axios';
 
 const DeckWrapper = () => {
-    const [ deckVisible, setDeckVisible ] = useState(true);
-    const [ deckId, setDeckId ] = useState('f1678z6x12j7');
+    // const [ currentCard, setCurrentCard ] = useState(null)
+    const [ autoDraw, setAutoDraw ] = useState(false);
+    const [ drawnCards, setDrawnCards ] = useState([]);
+    const [ deck, setDeck ] = useState('f1678z6x12j7');
+    const timer = useRef(null);
+
+
     useEffect(() => {
-        async function getDeckId() {
+        async function getDeck() {
             const res = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-            setDeckId(res.data.deck_id);
+            setDeck(res.data);
         }
-        getDeckId()
-    }, [])
+        getDeck()
+    }, [setDeck])
+
+    useEffect(() => {
+        async function getCard() {
+            let deckId = deck.deck_id;
+            console.log(deckId)
+
+            try {
+                let res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+
+                if (res.data.remaining === 0) {
+                    setAutoDraw(false);
+                    throw new Error("End of the deck!");
+                }
+
+                const card = res.data.cards[0];
+
+                setDrawnCards(c => [...c, { id: card.code, image: card.image }]);
+
+            } catch(e) {
+                alert(e)
+            }
+        }
+
+        if (autoDraw && !timer.current) {
+            timer.current = setInterval( async () => {
+                await getCard();
+            }, 1000);
+        }
+            
+        return () => {
+            clearInterval(timer.current);
+            timer.current = null;
+        };
+    }, [autoDraw, setAutoDraw, deck])
+    
+    const toggleAutoDraw = () => {
+        setAutoDraw(auto => !auto);
+    };
+
+    const cards = drawnCards.map(c => (<Deck key={c.id} image={c.image}/>));
+
+
 
     return (
-        <div>
-            <Deck deckId={deckId} />
+        <div className='DeckWrapper-deck'>
+            {deck ? (<button onClick={toggleAutoDraw}>
+                {autoDraw ? 'Stop drawing for me.' : 'Draw for me!'} 
+                </button>
+            ) : null}
+            <div className='DeckWrapper-card'>{cards}</div>
         </div>
-    )
+    );
 }
 
 export default DeckWrapper;
